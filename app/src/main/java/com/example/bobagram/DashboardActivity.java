8d8dda2766cfc53b7fa132d19adb578f889b0190
jessephams <jessephams@gmail.com>
2022-03-10 21:50:36 -0800

    Edit Profile Page

--- app/src/main/java/com/example/bobagram/DashboardActivity.java
+++ app/src/main/java/com/example/bobagram/DashboardActivity.java
@@ -2,7 +2,10 @@ package com.example.bobagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        Button btn = (Button)findViewById(R.id.open_activity_button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DashboardActivity.this, EditProfilePage.class));
+            }
+        });
     }
 }
