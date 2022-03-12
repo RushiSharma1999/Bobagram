46bf8e53e891c2d811d3a8e816c671945ae5338b
Rushi Sharma <rushisharma64@gmail.com>
2022-03-12 08:39:07 -0800

    added Login Activity

--- app/src/main/java/com/example/bobagram/LoginActivity.java
+++ app/src/main/java/com/example/bobagram/LoginActivity.java
@@ -1,14 +1,203 @@
 package com.example.bobagram;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.HashMap;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView createNewAcct, forgotPassword;
+    private Button loginButton;
+    private EditText emailEntered, passwordEntered;
+    private ProgressDialog loadingBar;
+    private FirebaseAuth authenticate;
+    FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Create Account");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+        createNewAcct = findViewById(R.id.createNewAccount);
+        forgotPassword = findViewById(R.id.forgotPassword);
+        loginButton = findViewById(R.id.loginButton);
+        emailEntered = findViewById(R.id.enterEmail);
+        passwordEntered = findViewById(R.id.enterPassword);
+        loadingBar = new ProgressDialog(this);
+        authenticate = FirebaseAuth.getInstance();
+
+        currentUser = authenticate.getCurrentUser();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailAdds = emailEntered.getText().toString().trim();
+                String pswrd = passwordEntered.getText().toString().trim();
+
+                if(!Patterns.EMAIL_ADDRESS.matcher(emailAdds).matches())
+                {
+                    emailEntered.setError("Invalid Email");
+                    emailEntered.setFocusable(true);
+                }
+                else
+                {
+                    loginUser(emailAdds, pswrd);
+                }
+            }
+        });
+
+        createNewAcct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
+            }
+        });
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showForgotPasswordDialog();
+            }
+        });
+    }
+
+    private void sendResetEmail(String emailAdds) {
+        loadingBar.setMessage("Sending Email....");
+        loadingBar.setCanceledOnTouchOutside(false);
+        loadingBar.show();
+
+        // send reset password email
+        authenticate.sendPasswordResetEmail(emailAdds).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                loadingBar.dismiss();
+                if (task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "Done sent", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Error Occured", Toast.LENGTH_LONG).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                loadingBar.dismiss();
+                Toast.makeText(LoginActivity.this, "Error Failed", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void showForgotPasswordDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Recover Password");
+        LinearLayout linearLayout = new LinearLayout(this);
+        //enter your account email address
+        final EditText emailField = new EditText(this);
+        emailField.setText("Email");
+        emailField.setMinEms(16);
+        emailField.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        linearLayout.addView(emailField);
+        linearLayout.setPadding(10, 10, 10, 10);
+        builder.setView(linearLayout);
+        builder.setPositiveButton("Recover", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                String emailAddss = emailField.getText().toString().trim();
+                //send a email to recover the password
+                sendResetEmail(emailAddss);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                dialog.dismiss();
+            }
+        });
+        builder.create().show();
+    }
+
+    private void loginUser(String emailAdds, String pswrd) {
+        loadingBar.setMessage("Logging In.....");
+        loadingBar.show();
+
+        authenticate.signInWithEmailAndPassword(emailAdds, pswrd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+
+                    loadingBar.dismiss();
+                    FirebaseUser user = authenticate.getCurrentUser();
+                    if (task.getResult().getAdditionalUserInfo().isNewUser())
+                    {
+                        String email = user.getEmail();
+                        String uid = user.getUid();
+                        HashMap<Object, String> hashMap = new HashMap<>();
+                        hashMap.put("email", email);
+                        hashMap.put("uid", uid);
+                        hashMap.put("name", "");
+                        hashMap.put("onlineStatus", "online");
+                        hashMap.put("typingTo", "noOne");
+                        hashMap.put("phone", "");
+                        hashMap.put("image", "");
+                        hashMap.put("cover", "");
+                        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+                        //store the value in Database in the "Users" Node
+                        DatabaseReference reference = database.getReference("Users");
+
+                        //storing the value in Firebase
+                        reference.child(uid).setValue(hashMap);
+                    }
+                    Toast.makeText(LoginActivity.this, "Registered User " + user.getEmail(), Toast.LENGTH_LONG).show();
+                    Intent mainIntent = new Intent(LoginActivity.this, DashboardActivity.class);
+                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(mainIntent);
+                    finish();
+                }
+                else
+                {
+                    loadingBar.dismiss();
+                    Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_LONG).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                loadingBar.dismiss();
+                Toast.makeText(LoginActivity.this, "Error Occured", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
