cbe15a9443c00f0920ce61d4e5951d07d79d3312
Spencer Siu <spencersiuu@gmail.com>
2022-03-12 17:12:41 -0800

    registration

--- app/src/main/java/com/example/bobagram/RegistrationActivity.java
+++ app/src/main/java/com/example/bobagram/RegistrationActivity.java
@@ -1,14 +1,38 @@
 package com.example.bobagram;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class RegistrationActivity extends AppCompatActivity {
+
+    FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
     }
+
+    mAuth.createUserWithEmailAndPassword(emaill, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        @Override
+        public void onComplete(@NonNull Task<AuthResult> task) {
+            if (task.isSuccessful()){
+                progressDialog.dismiss();
+            }
+            else {
+                progressDialog.dismiss();
+                Toast.makeText(RegistrationActivity.this,"Error",Toast.LENGTH_LONG).show();
+            }
+        }
+    }).addOnFailureListener(new OnFailureListener() {
+        @Override
+        public void onFailure(@NonNull Exception e) {
+            progressDialog.dismiss();
+            Toast.makeText(RegistrationActivity.this,"Error Occured",Toast.LENGTH_LONG).show();
+        }
+    });
 }
