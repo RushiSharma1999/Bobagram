8e308c8157d9f7faa54432848d5134238d0b0b81
Rushi Sharma <rushisharma64@gmail.com>
2022-03-26 17:08:27 -0700

    Minor Changes

--- app/src/main/java/com/example/bobagram/HomeFragment.java
+++ app/src/main/java/com/example/bobagram/HomeFragment.java
@@ -36,35 +36,27 @@ public class HomeFragment extends Fragment {
     RecyclerView recyclerView;
     List<ModelPost> posts;
     AdapterPosts adapterPosts;
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM1 = "param1";
-//    private static final String ARG_PARAM2 = "param2";
-//
-//    // TODO: Rename and change types of parameters
-//    private String mParam1;
-//    private String mParam2;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-//    public static HomeFragment newInstance(String param1, String param2) {
-//        HomeFragment fragment = new HomeFragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        firebaseAuth = FirebaseAuth.getInstance();
+        recyclerView = view.findViewById(R.id.postrecyclerview);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(layoutManager);
+        posts = new ArrayList<>();
+        loadPosts();
+        return view;
+    }
 
     private void loadPosts() {
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Posts");
@@ -92,22 +84,5 @@ public class HomeFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         setHasOptionsMenu(true);
         super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-        firebaseAuth = FirebaseAuth.getInstance();
-        recyclerView = view.findViewById(R.id.postrecyclerview);
-        recyclerView.setHasFixedSize(true);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        layoutManager.setReverseLayout(true);
-        layoutManager.setStackFromEnd(true);
-        recyclerView.setLayoutManager(layoutManager);
-        posts = new ArrayList<>();
-        loadPosts();
-        return view;
     }
 }
