0b29a288257e6aaecaa9462e75785c5743704f5c
Rushi Sharma <rushisharma64@gmail.com>
2022-04-29 18:11:08 -0700

    Minor Changes

--- app/src/main/java/com/example/bobagram/LoginActivity.java
+++ app/src/main/java/com/example/bobagram/LoginActivity.java
@@ -20,6 +20,7 @@ import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
